7245ae4c92711cad9ca66f51b2382f14d0d8c9ae
Hwang-Lan24423 <90197368+Hwang-Lan24423@users.noreply.github.com>
2024-12-24 16:32:16 +0700

    Add files via upload

---
+++ models/events.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title: String,
+    start: Date,
+    end: Date,
+    details: String,
+    status: Boolean,
+    more: Array,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Sử dụng kiểu ObjectId để tham chiếu đến ID của người dùng
+        ref: 'User'
+    }
+});
+
+const Event = mongoose.model('Event', eventSchema);
+module.exports = Event; 
---
+++ models/users.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+
+    },
+    email: {
+        type: String,
+    },
+    role: {
+        type: Boolean,
+        default: false 
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
